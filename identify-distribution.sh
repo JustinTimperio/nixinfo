10ecd5d0acd63212a3f8f3a8f91801fb1f9e00cf
Justin Timperio <justintimperio@gmail.com>
2020-10-12 14:16:49 -0400

    Fixing type check of package manager file

--- identify-distribution.sh
+++ identify-distribution.sh
@@ -73,36 +73,36 @@ identify_pkg_manager(){
 
   # SUSE Based
   elif [ -f "/usr/bin/zypper" ]; then
-    type=$(file /usr/bin/zypper --mime-type | awk -F '[ ]' '{ print $2 }')
-    if [ "$type" != "text/x-shellscript" -o "$type" != "text/x-perl" -o "$type" != "inode/symlink" ]; then
+    type=$(file /usr/bin/zypper --mime-type | awk -F '[ /]' '{ print $6 }')
+    if [ "$type" != "x-shellscript" && "$type" != "x-perl" && "$type" != "symlink" ]; then
       pkg_manager="zypper"
     fi
   
   # Deb Based
   elif [ -f "/usr/bin/apt" ] || [ -f "/bin/apt" ]; then
-    type=$(file /usr/bin/apt --mime-type | awk -F '[ ]' '{ print $2 }')
-    if [ "$type" != "text/x-shellscript" -o "$type" != "text/x-perl" -o "$type" != "inode/symlink" ]; then
+    type=$(file /usr/bin/apt --mime-type | awk -F '[ /]' '{ print $6 }')
+    if [ "$type" != "x-shellscript" && "$type" != "x-perl" && "$type" != "symlink" ]; then
       pkg_manager="apt"
     fi
 
   # RHL Based
   elif [ -f "/usr/bin/yum" ] || [ -f "/bin/yum" ]; then
-    type=$(file /usr/bin/yum --mime-type | awk -F '[ ]' '{ print $2 }')
-    if [ "$type" != "text/x-shellscript" -o "$type" != "text/x-perl" -o "$type" != "inode/symlink" ]; then
+    type=$(file /usr/bin/yum --mime-type | awk -F '[ /]' '{ print $6 }')
+    if [ "$type" != "x-shellscript" && "$type" != "x-perl" && "$type" != "symlink" ]; then
       pkg_manager="yum"
     fi
 
   # Arch Based
   elif [ -f "/usr/bin/pacman" ] || [ -f "/bin/pacman" ]; then
-    type=$(file /usr/bin/pacman --mime-type | awk -F '[ ]' '{ print $2 }')
-    if [ "$type" != "text/x-shellscript" -o "$type" != "text/x-perl" -o "$type" != "inode/symlink" ]; then
+    type=$(file /usr/bin/pacman --mime-type | awk -F '[ /]' '{ print $6 }')
+    if [ "$type" != "x-shellscript" && "$type" != "x-perl" && "$type" != "symlink" ]; then
       pkg_manager="pacman"
     fi
 
   # FreeBSD
   elif [ -f "/usr/sbin/pkg" ]; then
-    type=$(file /usr/sbin/pkg --mime-type | awk -F '[ ]' '{ print $2 }')
-    if [ "$type" != "text/x-shellscript" -o "$type" != "text/x-perl" -o "$type" != "inode/symlink" ]; then
+    type=$(file /usr/bin/pkg --mime-type | awk -F '[ /]' '{ print $6 }')
+    if [ "$type" != "x-shellscript" && "$type" != "x-perl" && "$type" != "symlink" ]; then
       pkg_manager="pkg"
     fi
 
